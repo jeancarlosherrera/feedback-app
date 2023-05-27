241fefc547fea759913dc19ca2f337d9b5561c65
Jean C. Herrera <jeancarlosherreram@gmail.com>
2023-05-27 11:31:22 -0400

    Feedback Item component added

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,10 @@
+const FeedbackItem = () => {
+  return (
+    <div className='card'>
+      <div className='num-display'>10</div>
+      <div className='text-display'>Example</div>
+    </div>
+  )
+}
+
+export default FeedbackItem
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
-const Header = ({ textColor, bgColor }) => {
+const Header = ({ textColor = '#ff6a95', bgColor = 'rgba(0, 0, 0, 0.4)' }) => {
   return (
-    <header style={{ color: textColor, backgroundColor: bgColor }}>
+    <header
+      style={{
+        color: textColor,
+        backgroundColor: bgColor,
+      }}
+    >
       <div className='container'>
         <h2>Feedback UI</h2>
       </div>
     </header>
   )
-}
-
-Header.defaultProps = {
-  bgColor: 'rgba(0, 0, 0, 0.4)',
-  textColor: '#ff6a95',
 }
 
 export default Header
