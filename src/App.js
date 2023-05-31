4cdd5645bebc6ee2ee6ae6a2264aa627839bbe7e
Jean C. Herrera <jeancarlosherreram@gmail.com>
2023-05-30 21:15:52 -0400

    About page and React Router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { v4 as uuidv4 } from 'uuid'
 import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import FeedbackData from './data/FeedbackData'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+import AboutPage from './pages/AboutPage'
 
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -21,12 +23,28 @@ const App = () => {
 
   return (
     <>
-      <Header />
-      <div className='container'>
-        <FeedbackForm onAddProp={addFeedback} />
-        <FeedbackStats feedbackProp={feedback} />
-        <FeedbackList feedbackProp={feedback} onDeleteProp={deleteFeedback} />
-      </div>
+      <Router>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route
+              exact
+              path='/'
+              element={
+                <>
+                  <FeedbackForm onAddProp={addFeedback} />
+                  <FeedbackStats feedbackProp={feedback} />
+                  <FeedbackList
+                    feedbackProp={feedback}
+                    onDeleteProp={deleteFeedback}
+                  />
+                </>
+              }
+            ></Route>
+            <Route path='/about' element={<AboutPage />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   )
 }
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,23 @@
+import Card from '../components/shared/Card'
+
+const AboutPage = () => {
+  return (
+    <Card>
+      <div className='about'>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+          necessitatibus tempore optio eligendi earum eius cum. Totam
+          exercitationem obcaecati harum eos voluptatibus perferendis minus quos
+          facilis. Rerum ipsum impedit minus.
+        </p>
+        <p>Version 1.0.0</p>
+        <p>
+          <a href='/'>Back to Home</a>
+        </p>
+      </div>
+    </Card>
+  )
+}
+
+export default AboutPage
